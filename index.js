b8bbc1e74542a34b1b54e336bf3722f32506dbaf
mitchelleokemwa <mitchelleokemwa@gmail.com>
2022-12-03 22:30:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
+  if (feet <= 400) {
+    return "This one is on me!";
+  }
+  if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    return "No can do.";
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+    if (city === 'NYC'){
+    return 'Ok, sounds good.'
+  }else {
+    return 'No go.'}
+
+  // return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  } else if (tip === "not as generous") {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
